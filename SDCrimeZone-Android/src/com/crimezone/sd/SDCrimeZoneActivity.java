2a65c3e0efeb3e00131ba395cda2c6c55a2e0e4d
chippoc <chippoc@gmail.com>
2012-03-06 21:02:46 -0800

    adding HTTPRequest for Android client

--- SDCrimeZone-Android/src/com/crimezone/sd/SDCrimeZoneActivity.java
+++ SDCrimeZone-Android/src/com/crimezone/sd/SDCrimeZoneActivity.java
@@ -1,13 +1,25 @@
 package com.crimezone.sd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.content.Context;
@@ -15,12 +27,15 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,11 +49,13 @@ import android.widget.TextView;
 public class SDCrimeZoneActivity extends Activity implements View.OnClickListener {
 
   public static Location currLocation;
-  
-  public static final Map<String,String> bccMap;
+  public static String selectedRadius;
+  public static String selectedDate;
+
+  public static final Map<String, String> bccMap;
   static {
     Map<String, String> aMap = new HashMap<String, String>();
-    aMap.put("1","Murder");
+    aMap.put("1", "Murder");
     aMap.put("2", "Rape");
     aMap.put("3", "Robbery");
     aMap.put("4", "Assault");
@@ -69,21 +86,44 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
   }
 
   /**
-   * initializeApp Take user to landing page for SD Crime Zone. Populate the
-   * dropdown boxes (Distance and Date)
+   * Take user to landing page for SD Crime Zone. Populate the dropdown boxes
+   * (Distance and Date)
    */
   public void initializeApp() {
     setContentView(R.layout.main);
     // get current GPS coordinates and set as default for app
     EditText currLocationText = (EditText) this.findViewById(R.id.addressText);
-    currLocationText.setText("Current Location");
+    currLocationText.setText(getString(R.string.defaultLocation));
     // the distance dropdown list
     Spinner distanceList = (Spinner) this.findViewById(R.id.distanceList);
     populateSpinnerWithArray(distanceList, R.array.distanceArray);
+    distanceList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        selectedRadius = parent.getSelectedItem().toString();
+        selectedRadius = selectedRadius.replaceFirst("\\smile", "");
+      }
+
+      public void onNothingSelected(AdapterView<?> parent) {
+        parent.setSelection(0);
+        selectedRadius = parent.getSelectedItem().toString();
+        selectedRadius = selectedRadius.replaceFirst("\\smile", "");
+      }
+    });
+
     // the distance dropdown list
     Spinner dateList = (Spinner) this.findViewById(R.id.datesList);
     populateSpinnerWithArray(dateList, R.array.dateArray);
-    Button submitButton = (Button)this.findViewById(R.id.submitButton);
+    dateList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        selectedDate = parent.getSelectedItem().toString();
+      }
+
+      public void onNothingSelected(AdapterView<?> parent) {
+        parent.setSelection(0);
+        selectedDate = parent.getSelectedItem().toString();
+      }
+    });
+    Button submitButton = (Button) this.findViewById(R.id.submitButton);
     submitButton.setOnClickListener(this);
   }
 
@@ -116,9 +156,13 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
   }
 
   public void updateAddressWithCurrentLocation(Location location) {
-    currLocation = location;
-    //this.debug("Location= " + String.valueOf(location.getLatitude())
-    //    + ", " + String.valueOf(location.getLongitude()));
+    EditText addrText = (EditText) this.findViewById(R.id.addressText);
+    if (addrText != null
+        && addrText.getText().toString().equals(getString(R.string.defaultLocation))) {
+      currLocation = location;
+    }
+    // this.debug("Location= " + String.valueOf(location.getLatitude())
+    // + ", " + String.valueOf(location.getLongitude()));
   }
 
   /**
@@ -135,7 +179,7 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     spinner.setAdapter(adapter);
   }
-  
+
   private void debug(String out) {
     Log.v(getString(R.string.app_name), out);
   }
@@ -147,9 +191,9 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
    */
   public void onClick(View v) {
     if (v.getId() == R.id.submitButton) {
-      ArrayList<JSONObject> results = this.sendHttpRequestToServer(v);
+      JSONArray results = this.sendHttpRequestToServer(v);
       this.debug("got results, switching layout");
-      //switch to the results page
+      // switch to the results page
       try {
         populateResultsPage(results);
       } catch (JSONException e) {
@@ -157,85 +201,141 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
         e.printStackTrace();
       }
     }
-    
+
   }
-  
-  private void populateResultsPage(ArrayList<JSONObject> results) throws JSONException {
+
+  private void populateResultsPage(JSONArray results) throws JSONException {
     setContentView(R.layout.crimes);
-    TableLayout myLayout = (TableLayout)this.findViewById(R.id.crimesLayout);
+    TableLayout myLayout = (TableLayout) this.findViewById(R.id.crimesLayout);
     HashMap<String, Integer> incidentMap = new HashMap<String, Integer>();
-    for (JSONObject obj: results) {
-      String bcc = (String) obj.get("bcc");
-      if (!incidentMap.containsKey(bcc)) {
-        incidentMap.put(bcc, Integer.valueOf(1));
-      } else {
-        Integer num = incidentMap.get(bcc);
-        int newNum = num.intValue() + 1;
-        incidentMap.put(bcc, new Integer(newNum));
+
+    if (results == null) {
+      // TODO: display an elegent error message, if no results found
+      // return user to main
+    } else {
+      for (int i = 0; i < results.length(); i++) {
+        JSONObject obj = results.getJSONObject(i);
+        String bcc = (String) obj.get("bcc");
+        if (!incidentMap.containsKey(bcc)) {
+          incidentMap.put(bcc, Integer.valueOf(1));
+        } else {
+          Integer num = incidentMap.get(bcc);
+          int newNum = num.intValue() + 1;
+          incidentMap.put(bcc, new Integer(newNum));
+        }
+      }
+
+      for (String bcc : incidentMap.keySet()) {
+        this.debug("added " + bcc);
+        /* Create a new row to be added. */
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        /* Create the Crime text to be in the row-content. */
+        TextView t = new TextView(this);
+        t.setText(bccMap.get(bcc)); // get incident type, based on bcc code
+        LayoutParams params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+        params.setMargins(30, 15, 30, 15);
+        t.setLayoutParams(params);
+
+        /* Add text to row. */
+        tr.addView(t);
+        /* Create the bar image to be added */
+        ImageView i = new ImageView(this);
+
+        Resources res = getResources();
+        // get the bar .png image
+        Drawable drawable = res.getDrawable(R.drawable.bar);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        // crop the image based on how many incidents of that type
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, 10 * incidentMap.get(bcc).intValue(),
+            bitmap.getHeight());
+        this.debug("height= " + bitmap.getHeight());
+
+        i.setImageBitmap(bitmap);
+        i.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        /* Add image to row */
+        tr.addView(i);
+        myLayout.addView(tr, new TableLayout.LayoutParams(LayoutParams.FILL_PARENT,
+            LayoutParams.WRAP_CONTENT));
       }
     }
-    
-    for (String bcc: incidentMap.keySet()) {
-      this.debug("added " + bcc);
-      /* Create a new row to be added. */
-      TableRow tr = new TableRow(this);
-      tr.setLayoutParams(new LayoutParams(
-          LayoutParams.FILL_PARENT,
-          LayoutParams.WRAP_CONTENT));
-           /* Create the Crime text to be in the row-content. */
-           TextView t = new TextView(this);
-           t.setText(bccMap.get(bcc));  //get incident type, based on bcc code
-           LayoutParams params = new LayoutParams(
-               LayoutParams.FILL_PARENT,
-               LayoutParams.WRAP_CONTENT);
-           params.setMargins(30, 15, 30, 15);
-           t.setLayoutParams(params);
 
-           /* Add text to row. */
-           tr.addView(t);
-           /* Create the bar image to be added */
-           ImageView i = new ImageView(this);
-           
-           Resources res = getResources();
-           //get the bar .png image
-           Drawable drawable = res.getDrawable(R.drawable.bar);
-           Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-           //crop the image based on how many incidents of that type
-           bitmap = Bitmap.createBitmap(bitmap,0,0,10*incidentMap.get(bcc).intValue(),bitmap.getHeight());
-           this.debug("height= " + bitmap.getHeight());
-
-           i.setImageBitmap (bitmap);
-           i.setLayoutParams(new LayoutParams(
-               LayoutParams.FILL_PARENT,
-               LayoutParams.WRAP_CONTENT));
-           /* Add image to row */
-           tr.addView(i);
-           myLayout.addView(tr, new TableLayout.LayoutParams(
-               LayoutParams.FILL_PARENT,
-               LayoutParams.WRAP_CONTENT));
-    }
-    
   }
 
-  private ArrayList<JSONObject> sendHttpRequestToServer(View v) {
+  /**
+   * Sends an HttpRequest to SDCrimeZone-AppEngine with lat, long, radius (and
+   * date). Gets the JSON result and returns it. Should ONLY be called from main
+   * page.
+   * 
+   **/
+  private JSONArray sendHttpRequestToServer(View v) {
     // get the current GPS coordinates, distance, and dates selected
-    ArrayList<JSONObject> jsonObjs = new ArrayList<JSONObject>();
-    Random rand = new Random();
-    //temp: create some fake data
-    for (int i = 0; i < 30; i++) {
-      JSONObject ret = new JSONObject();
-      try {
-        ret.put("address", "4496 Park Boulevard, San Diego");
-        ret.put("bcc", String.valueOf(rand.nextInt(8) + 1));
-        jsonObjs.add(ret);
-      } catch (JSONException e) {
-        e.printStackTrace();
-      }
+    /*
+     * ArrayList<JSONObject> jsonObjs = new ArrayList<JSONObject>(); Random rand
+     * = new Random(); //temp: create some fake data for (int i = 0; i < 30;
+     * i++) { JSONObject ret = new JSONObject(); try { ret.put("address",
+     * "4496 Park Boulevard, San Diego"); ret.put("bcc",
+     * String.valueOf(rand.nextInt(8) + 1)); jsonObjs.add(ret); } catch
+     * (JSONException e) { e.printStackTrace(); } }
+     */
+    EditText addr = (EditText) this.findViewById(R.id.addressText);
+    Spinner dist = (Spinner) this.findViewById(R.id.distanceList);
+    String currentAddress = addr.getText().toString();
+    double[] latlong = { currLocation.getLatitude(), currLocation.getLongitude() };
+    this.debug("curr latlong = " + latlong[0] + ", " + latlong[1]);
+    if (!currentAddress.equals(getString(R.string.defaultLocation))) {
+      this.debug("updating address: " + currentAddress);
+      latlong = getLatLong(currentAddress);
     }
-    
-   
-    
-    
-    return jsonObjs;
+    HttpResponse response;
+    try {
+      HttpClient hc = new DefaultHttpClient();
+      HttpGet get = new HttpGet("http://sdcrimezone.appspot.com/?lat=" + latlong[0]
+          + "&lng=" + latlong[1] + "&rad=" + selectedRadius);
+
+      this.debug("HTTPGet = http://sdcrimezone.appspot.com/?lat=" + String.valueOf(latlong[0])
+          + "&lng=" + String.valueOf(latlong[1]) + "&rad=" + selectedRadius);
+
+      response = hc.execute(get);
+
+      // get the response from the Google Apps Engine server, should be in JSON
+      // format
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        Reader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent(),
+            "UTF-8"));
+        StringBuilder builder = new StringBuilder();
+        char[] buf = new char[1000];
+        int l = 0;
+        while (l >= 0) {
+          builder.append(buf, 0, l);
+          l = in.read(buf);
+        }
+        JSONTokener tokener = new JSONTokener(builder.toString());
+        JSONArray finalResult = new JSONArray(tokener);
+        return finalResult;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
   }
+
+  private double[] getLatLong(String strAddress) {
+    Geocoder coder = new Geocoder(this);
+    List<Address> address;
+
+    try {
+      address = coder.getFromLocationName(strAddress, 5);
+      if (address == null) {
+        return null;
+      }
+      Address location = address.get(0);
+      double[] results = { location.getLatitude(), location.getLongitude() };
+      return results;
+    } catch (Exception e) {
+      return null;
+    }
+
+  }
+
 }
