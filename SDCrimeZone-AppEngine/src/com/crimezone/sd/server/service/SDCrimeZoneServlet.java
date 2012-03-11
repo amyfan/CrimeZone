a60df4b4c023b4527ece67284503d1fc7a76df44
grark <stjarnljuset@gmail.com>
2012-03-11 15:17:32 -0700

    load all incidents

--- SDCrimeZone-AppEngine/src/com/crimezone/sd/server/service/SDCrimeZoneServlet.java
+++ SDCrimeZone-AppEngine/src/com/crimezone/sd/server/service/SDCrimeZoneServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.crimezone.sd.server.logic.CrimeDataLoader;
 
+
 public class SDCrimeZoneServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
@@ -24,18 +25,34 @@ public class SDCrimeZoneServlet extends HttpServlet {
         String rad = checkNull(req.getParameter("rad"));
         
         resp.getWriter().println("lat = " + lat + ", lng = " + lng + ", rad = " + rad);
-
-        String url = "http://localhost:8888/resources/testnext5.txt";
-        resp.getWriter().println(url);
         
-        URL inputData = new URL(url);
-        URLConnection  urlConn = inputData.openConnection();
-        InputStreamReader is = new InputStreamReader(urlConn.getInputStream(), "UTF8");
-        BufferedReader in = new BufferedReader(is);
-        CrimeDataLoader dataLoader = CrimeDataLoader.getInstance();
-        dataLoader.insertIncidents(in);
-
+        String action = checkNull(req.getParameter("action"));
+        
+        if (action.compareToIgnoreCase("load") == 0) {
+          String serverName = req.getServerName();
+          String url;
+          if (serverName.compareToIgnoreCase("127.0.0.1") == 0 || serverName.compareToIgnoreCase("localhost") == 0)
+            url = String.format("http://%s:8888/resources/complete.txt", serverName);
+          else
+            url = "http://sdcrimezone.appspot.com:8888/resources/complete.txt";
+          LoadFile(url);
+        }
+        
     }
+	
+	private void LoadFile(String url) {
+	  try {
+          URL inputData = new URL(url);
+          URLConnection  urlConn = inputData.openConnection();
+          InputStreamReader is = new InputStreamReader(urlConn.getInputStream(), "UTF8");
+          BufferedReader in = new BufferedReader(is);
+          CrimeDataLoader dataLoader = CrimeDataLoader.getInstance();
+          dataLoader.deleteAllIncidents();
+          dataLoader.insertIncidents(in);
+	  }
+	  catch (Exception e) { }
+	  finally { }
+	}
 	
 	private String checkNull(String s) {
 		if (s == null) {
