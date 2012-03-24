592fed6a4b4d0db4e6268292aa986ac3e7297a90
chippoc <chippoc@gmail.com>
2012-03-23 17:31:54 -0700

    enabling connection to actual google server, disabling random data

--- SDCrimeZone-Android/src/com/crimezone/sd/SDCrimeZoneActivity.java
+++ SDCrimeZone-Android/src/com/crimezone/sd/SDCrimeZoneActivity.java
@@ -1,11 +1,17 @@
 package com.crimezone.sd;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
-import java.util.Random;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.content.Context;
@@ -189,49 +195,48 @@ public class SDCrimeZoneActivity extends Activity implements View.OnClickListene
     // get the current GPS coordinates, distance, and dates selected
 
     JSONArray jsonObjs = new JSONArray();
-    Random rand = new Random(); // temp: create some fake data
-    for (int i = 0; i < 30; i++) {
-      JSONObject ret = new JSONObject();
-      try {
-        ret.put("address", "4496 Park Boulevard, San Diego");
-        ret.put("bcc", String.valueOf(rand.nextInt(8) + 1));
-        ret.put("lng", "-117.2025731");
-        ret.put("lat", "32.9035252");
-        jsonObjs.put(ret);
-      } catch (JSONException e) {
-        e.printStackTrace();
-      }
+
+    EditText addr = (EditText) this.findViewById(R.id.addressText);
+    Spinner dist = (Spinner) this.findViewById(R.id.distanceList);
+    String currentAddress = addr.getText().toString();
+    double[] latlong = { currLocation.getLatitude(), currLocation.getLongitude() };
+    if (!currentAddress.equals(getString(R.string.defaultLocation))) {
+      latlong = getLatLong(currentAddress);
     }
-    return jsonObjs;
-    /*
-     * EditText addr = (EditText) this.findViewById(R.id.addressText); Spinner
-     * dist = (Spinner) this.findViewById(R.id.distanceList); String
-     * currentAddress = addr.getText().toString(); double[] latlong = {
-     * currLocation.getLatitude(), currLocation.getLongitude() };
-     * this.debug("curr latlong = " + latlong[0] + ", " + latlong[1]); if
-     * (!currentAddress.equals(getString(R.string.defaultLocation))) {
-     * this.debug("updating address: " + currentAddress); latlong =
-     * getLatLong(currentAddress); } HttpResponse response; try { HttpClient hc
-     * = new DefaultHttpClient(); HttpGet get = new
-     * HttpGet("http://sdcrimezone.appspot.com/crimeZoneServlet?lat=" +
-     * latlong[0] + "&lng=" + latlong[1] + "&rad=" + selectedRadius);
-     * 
-     * this.debug("HTTPGet = http://sdcrimezone.appspot.com/crimeZoneServlet?lat="
-     * + String.valueOf(latlong[0]) + "&lng=" + String.valueOf(latlong[1]) +
-     * "&rad=" + selectedRadius);
-     * 
-     * response = hc.execute(get);
-     * 
-     * // get the response from the Google Apps Engine server, should be in JSON
-     * // format if (response.getStatusLine().getStatusCode() ==
-     * HttpStatus.SC_OK) { Reader in = new BufferedReader(new
-     * InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-     * StringBuilder builder = new StringBuilder(); char[] buf = new char[1000];
-     * int l = 0; while (l >= 0) { builder.append(buf, 0, l); l = in.read(buf);
-     * } JSONTokener tokener = new JSONTokener(builder.toString()); JSONArray
-     * finalResult = new JSONArray(tokener); return finalResult; } } catch
-     * (Exception e) { e.printStackTrace(); } return null;
-     */
+    HttpResponse response;
+    try {
+      HttpClient hc = new DefaultHttpClient();
+      HttpGet get = new HttpGet("http://sdcrimezone.appspot.com/crimeZoneServlet?lat=" + latlong[0]
+          + "&lng=" + latlong[1] + "&rad=" + selectedRadius + "&year=" + selectedDate);
+
+      SDCrimeZoneApplication.debug( this,
+          "HTTPGet = http://sdcrimezone.appspot.com/crimeZoneServlet?lat="
+              + String.valueOf(latlong[0]) + "&lng=" + String.valueOf(latlong[1]) + "&rad="
+              + selectedRadius + "&year=" + selectedDate);
+
+      response = hc.execute(get);
+
+      // get the response from the Google Apps Engine server, should be in JSON
+      // format
+      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+        Reader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent(),
+            "UTF-8"));
+        StringBuilder builder = new StringBuilder();
+        char[] buf = new char[1000];
+        int l = 0;
+        while (l >= 0) {
+          builder.append(buf, 0, l);
+          l = in.read(buf);
+        }
+        JSONTokener tokener = new JSONTokener(builder.toString());
+        JSONArray finalResult = new JSONArray(tokener);
+        return finalResult;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+
   }
 
   private double[] getLatLong(String strAddress) {
